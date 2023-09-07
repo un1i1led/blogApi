a98c2154a9b35d0e72a4e4a1771f0fcead72731a
gil <gsqwow@gmail.com>
2023-09-07 07:57:03 -0700

    Add isAdmin attr

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model('User', UserSchema);
