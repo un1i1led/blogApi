b46f910e24d5ee45e76bd79c82a5dacbc0e862a9
gil <gsqwow@gmail.com>
2023-09-07 07:57:29 -0700

    Add posts routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const postController = require('../controllers/postController');
+const userController = require('../controllers/userController');
+
+router.post('/posts', postController.post_add_post);
+router.get('/posts', postController.post_list_get);
+router.get('/posts/:postid', postController.post_get);
+
+router.post('/users', userController.user_create_post);
+
 /* GET home page. */
 router.get('/', (req, res) => {
     res.json({
