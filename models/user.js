7b3c82513cf805d3acb5e365ac0dadbb6575d443
gil <gsqwow@gmail.com>
2023-10-06 22:39:25 -0700

    Add email, name and isAuthor field to user model

--- models/user.js
+++ models/user.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    email: { type: String, required: true },
+    name: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true },
+    isAuthor: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model('User', UserSchema);
