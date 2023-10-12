9d156e3d9af62a01f015dc795e8969c81fd3914b
gil <gsqwow@gmail.com>
2023-10-12 14:59:15 -0700

    Add user attr to post model

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
     body: { type: String, required: true },
     date: { type: Date, required: true },
