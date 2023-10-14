29529e7df2bd67e317e01f27ee4e8f799673bf7c
gil <gsqwow@gmail.com>
2023-10-13 18:51:25 -0700

    Refactor tag_slider_list controller to return categories with newest articles

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,13 +1,6 @@
 const Tag = require('../models/tag');
+const Post = require('../models/post');
 const asyncHandler = require('express-async-handler');
-
-exports.tag_slider_list = asyncHandler(async(req, res, next) => {
-    const tags = await Tag.find({}).limit(3).exec();
-
-    res.json({
-        tags: tags
-    })
-})
 
 exports.tag_list_get = asyncHandler(async(req, res, next) => {
     const tags = await Tag.find({}).exec();
@@ -16,3 +9,16 @@ exports.tag_list_get = asyncHandler(async(req, res, next) => {
         tags: tags
     })
 }) 
+
+exports.tag_slider_list = asyncHandler(async(req, res, next) => {
+    const postOne = await Post.findOne({}).sort('-date').populate('tags');
+    const postTwo = await Post.findOne({ 'tags': { '$ne': postOne.tags._id }}).sort('-date').populate('tags');
+    const postThree = await Post.findOne({ 'tags': { '$nin': [postOne.tags._id, postTwo.tags._id] }}).sort('-date').populate('tags');
+
+    const tags = [postOne.tags, postTwo.tags, postThree.tags];
+
+    res.json({
+        tags
+    })
+    
+})
