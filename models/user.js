d721a785a0e7a157e7aa58d8f83e1e2f853bc9b2
gil <gsqwow@gmail.com>
2023-10-17 20:40:18 -0700

    Add img attribute to user model

--- models/user.js
+++ models/user.js
@@ -8,6 +8,7 @@ const UserSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     isAuthor: { type: Boolean, required: true },
+    img: { type: String, required: false }
 });
 
 module.exports = mongoose.model('User', UserSchema);
